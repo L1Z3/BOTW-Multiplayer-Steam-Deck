441d547a854e7ab9fec007700d0ea85353397521
L1Z3 <10276179+L1Z3@users.noreply.github.com>
2023-04-07 20:42:10 -0400

    Add download mod files functionality

--- main.py
+++ main.py
@@ -2,17 +2,54 @@ import os
 import shutil
 from steampy.shortcuts import create_shortcut
 from xml.etree import ElementTree as ET
+import requests
+from requests.structures import CaseInsensitiveDict
+import zipfile
+import uuid
+import os
+import json
+
+DOWNLOAD_URL = "https://api.github.com/repos/edgarcantuco/BOTW.Release/releases"
+WORKING_DIR = os.path.expanduser("~/.local/share/botwminstaller")
+MOD_DIR = os.path.join(WORKING_DIR, "BreathOfTheWildMultiplayer")
+
+def download_mod_files():
+    headers = CaseInsensitiveDict()
+    r = requests.get(DOWNLOAD_URL, headers=headers)
+    pretty = json.dumps(r.json(), indent=4)
+    r_json = r.json()
+    if len(r_json) == 0:
+        raise Exception("Error downloading mod files")
+    latest_release = r_json[0]
+    download_link = latest_release["assets"][0]["browser_download_url"]
+
+    r = requests.get(download_link, headers=headers)
+
+    zip_name = os.path.join(WORKING_DIR, uuid.uuid4().hex + ".zip")
+
+    if r.status_code == 200:
+        with open(zip_name, 'wb') as file:
+            file.write(r.content)
+
+        with zipfile.ZipFile(zip_name, 'r') as zip_ref:
+            zip_ref.extractall(MOD_DIR)
+
+        file.close()
+        zip_ref.close()
+
+        os.remove(zip_name)
+    else:
+        raise Exception("Error downloading mod files")
 
 def main(cemu_path):
     # Generate the working directory
-    working_dir = os.path.expanduser("~/.local/share/botwminstaller")
-    os.makedirs(working_dir, exist_ok=True)
+    os.makedirs(WORKING_DIR, exist_ok=True)
 
     # Download the latest mod files
-    # TODO: add code to download mod files
+    download_mod_files()
 
     # Create a Steam shortcut for 'Breath of the Wild Multiplayer.exe'
-    game_path = os.path.join(working_dir, "Breath of the Wild Multiplayer.exe")
+    game_path = os.path.join(MOD_DIR, "Breath of the Wild Multiplayer.exe")
     create_shortcut(game_path, "Breath of the Wild Multiplayer", cemu_path)
 
     # Get the id for the Steam shortcut
@@ -21,6 +58,8 @@ def main(cemu_path):
         contents = f.read()
         id = contents.split("steamid=")[1].split("&")[0]
 
+
+    # TODO install protontricks if not installed already?
     # Run protontricks to install dotnetcoredesktop6
     os.system(f"protontricks {id} install dotnetcoredesktop6")
 
